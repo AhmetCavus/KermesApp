90fd2f224b417769c1b9db8146241867b7ddd580
Ahmet Cavus <ahmet.cavus@yapptek.com>
2025-09-15 00:37:04 +0200

    chore: Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
-import { Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 
 const Header: React.FC = () => {
     return (
-        <>
-            <Typography variant="h4" gutterBottom marginTop={2}>
-                Kermes 2025 Menü
-            </Typography>
-        </>
-    )
+      <>
+        {/* Background Image */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            minHeight: "100vh", // full screen
+            backgroundColor: "#fefefe", // fallback color
+            backgroundImage: "url(/images/header.bg.png)",
+            backgroundRepeat: "repeat",
+            backgroundSize: "contain", // or "contain" / custom px
+            zIndex: -1,
+          }}
+        />
+        <Typography variant="h4" gutterBottom marginTop={2}>
+          Kermes 2025 Menü
+        </Typography>
+      </>
+    );
 }
 
 export default Header
